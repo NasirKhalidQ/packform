6e14645d208f1c12f7b7e52a79c51383315ab077
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-11-13 22:41:49 +0500

    added components for slecting input

--- vue-frontend/src/components/CustomerOrders.vue
+++ vue-frontend/src/components/CustomerOrders.vue
@@ -1,3 +1,156 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { ref } from "vue";
 
-<template><h1>hello</h1></template>
+export default {
+  components: { Datepicker },
+  setup() {
+    const date = ref(new Date());
+    const headers = [
+      {
+        text: "Dessert (100g serving)",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Iron (%)", value: "iron" },
+    ];
+    const desserts = [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: "1%",
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: "1%",
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: "7%",
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: "8%",
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: "16%",
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: "0%",
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: "2%",
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: "45%",
+      },
+      {
+        name: "Donut",
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: "22%",
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: "6%",
+      },
+    ];
+
+    return {
+      date,
+      headers,
+      desserts,
+    };
+  },
+};
+</script>
+
+<template>
+  <v-row>
+    <v-col cols="1">
+      <img
+        src="https://uxwing.com/wp-content/themes/uxwing/download/user-interface/search-icon.png"
+        alt="search"
+        width="30"
+        height="30"
+      />
+    </v-col>
+    <v-col cols="11">
+      <h2>Search</h2>
+    </v-col>
+  </v-row>
+  <v-text-field variant="outlined" />
+  <h6 class="text-h6">Created Date</h6>
+  <v-row>
+    <v-col :cols="3">
+      <Datepicker
+        v-model="date"
+        :month-change-on-scroll="false"
+        :range="true"
+        :enable-time-picker="false"
+        :no-today="true"
+      />
+    </v-col>
+  </v-row>
+  <v-table :height="500">
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in desserts" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
